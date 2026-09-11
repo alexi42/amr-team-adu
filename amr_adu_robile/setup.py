65e50a0f1c42e84fef1f6ac18ee19e8458287b30
Ulaş Turgut <ulas.turqut305@gmail.com>
2026-09-11 03:07:10 +0200

    Add path visualization for Rviz

---
+++ amr_adu_robile/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'amr_adu_robile'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='trgtulas',
+    maintainer_email='ulas.turqut305@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'state_machine = path_planning.pot_field_path_planner:main',
+        ],
+    },
+)
