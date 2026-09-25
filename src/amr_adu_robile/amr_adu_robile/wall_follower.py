007c89fa193d626fc7590b0f6dc1b1a2bb07be8e
Alex Zarkh <alexandra.zarkh@smail.inf.h-brs.de>
2026-09-25 21:02:20 +0200

    First draft of wall follower exploration

--- src/amr_adu_robile/amr_adu_robile/wall_follower.py
+++ src/amr_adu_robile/amr_adu_robile/wall_follower.py
@@ -6,17 +6,25 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, Pose, Point, Twist
 from tf_transformations import euler_from_quaternion
 
+# Code inspired by https://github.com/HBRS-AMR/Robile/blob/main/robile_navigation/robile_navigation_demo/ros/scripts/wall_follower.py
 
-class FrontierBasedExploration:
-    """Robot explores environment autonomously driving to the current range limits."""
+
+class WallFollwerExploration:
+    """
+    Robot explores environment autonomously by driving along the walls 
+    and then exploring the unknown cells in the occupancy grid map.
+    """
 
     def __init__(self, node):
         self.node = node
         self.latest_scan = None
-        self.robot_position = np.array([0.0 0.0])
+        self.robot_position = np.array([0.0, 0.0])
         self.robot_angle = 0.0
         self.already_visited = None
         self.next_goals = None
+        self.threshold_dist_wall = 1.0
+        self.max_linear_vel = 0.3
+        self.angular_velocity = 0.3
 
         # Subscribers
 
@@ -61,6 +69,25 @@ class FrontierBasedExploration:
     def scan_callback(self, msg):
         """Store latest laser scan data."""
         self.latest_scan = msg
+        self.latest_scan_cartesian = self.process_data(np.array(msg))
+
+    def process_data(self, scan):
+        """Calculate latest scan measurements to cartesian coorinates."""
+        scan_in_cartesian = []
+        for i, range_val in enumerate(scan.ranges):
+            # Skip invalid readings
+            if range_val < scan.range_min or range_val > scan.range_max or :
+                continue
+
+            # Calculate angle
+            angle = scan.angle_min + i * scan.angle_increment
+
+            # Convert to cartesian coordinates in base_link frame
+            x = range_val * np.cos(angle)
+            y = range_val * np.sin(angle)
+            scan_in_cartesian.append(np.array([x, y]))
+        return scan_in_cartesian
+
 
     def explore_environment(self):
         # check laser scan and odometry data
@@ -68,12 +95,36 @@ class FrontierBasedExploration:
         # reach next goal at range limit
         # calculate new goals at next range limit until whole environment is done
         # publish already visited poses continously for SLAM component to map
-        
+        if self.latest_scan is None:
+            return
+
+        current_robot_position = self.robot_position
+        next_goal = self.next_goals[0]
+        twist = Twist()
+        while len(self.next_goals) > 0:
+            if np.array_equal(current_robot_position, next_goal):
+                del self.next_goals[0]
+                self.already_visited = np.append(self.already_visited, current_robot_position)
+                print("Next goal was reached.")
+            else:
+                twist = self.drive_to_next_goal(next_goal)
+                self.cmd_vel_pub.publish(twist)
+        print("Environment completely explored!")
+
+    def drive_to_next_goal(self, next_goal, twist):
+        # check robot's current position
+        # check where the next goal is
+        # calculate 
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.angular.z = 0.0
+        return twist
+
 
 def main(args=None):
     rclpy.init()
 
-    frontier_exploration = FrontierBasedExploration()
+    frontier_exploration = WallFollwerExploration()
     executor = rclpy.get_global_executor()
     executor.add_node(frontier_exploration)
     try:
